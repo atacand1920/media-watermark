ee5568be3efb8d1357c9c72c59db82a520d9eff1
Seifeddine Bouzid <bouzid.saif@hotmail.com>
2022-04-15 18:55:51 +0200

    change to dynamic library

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "media-watermark",
+            type: .dynamic,
             targets: ["media-watermark"]),
-            .library(name: "media-watermarkTests",targets: ["media-watermarkTests"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
